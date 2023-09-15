2be8a26ca776139598f17ca411ab5754c2acc282
Friedrich ten Hagen <friedrichtenhagen@Friedrichs-MBP.fritz.box>
2023-09-15 17:52:35 +0200

    Solve school name problem.

--- groupby.sql
+++ groupby.sql
@@ -1,7 +1,16 @@
-SELECT CASE WHEN year IN ('FR', 'SO') THEN 'underclass'
-WHEN year IN ('JR', 'SR') THEN 'upperclass'
-ELSE NULL END AS class_group, 
-SUM(weight) AS combined_player_weight
-FROM benn.college_football_players
-WHERE state = 'CA'
-GROUP BY 1
+/* Write a query that shows the number of players at schools with names that start with A through M, and the number at schools with names starting with N - Z. */
+    /*Version 1*/
+    SELECT 
+    COUNT(CASE WHEN school_name < 'n' THEN 1 
+    ELSE NULL END) AS starts_with_r,
+    COUNT(CASE WHEN school_name >= 'n' THEN 1 
+    ELSE NULL END) AS starts_with_c
+    FROM benn.college_football_players
+    /*Version 2*/
+    SELECT 
+    CASE WHEN school_name < 'n' THEN 'a_m'
+            WHEN school_name >= 'n' THEN 'n_z'
+    END AS alphabetical_grouping,
+    COUNT(1) AS num_students
+    FROM benn.college_football_players
+    GROUP BY 1
