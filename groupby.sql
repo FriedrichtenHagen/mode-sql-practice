419445d4798963e181741bdc2371e55f276c8ac0
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-09-16 19:19:02 +0200

    Finish Group by problems.

--- groupby.sql
+++ groupby.sql
@@ -16,7 +16,23 @@
     GROUP BY 1
 
 
+/*
+Write a query that separately counts the number of unique values in the month column and the number of unique values in the `year` column.
+*/
 
-SELECT DISTINCT month, year
-FROM tutorial.aapl_historical_stock_price
-ORDER BY year desc, month desc
+SELECT 
+       COUNT(DISTINCT month) AS months_count, 
+       COUNT(DISTINCT year) AS years_count
+  FROM tutorial.aapl_historical_stock_price
+
+/*
+Write a query that selects the school name, player name, position, and weight for every player in Georgia, ordered by weight (heaviest to lightest). 
+Be sure to make an alias for the table, and to reference all column names in relation to the alias.
+*/
+
+SELECT players.full_school_name, players.player_name, players.position, players.weight
+FROM benn.college_football_players AS players
+JOIN benn.college_football_teams AS teams
+    ON teams.school_name = players.school_name
+WHERE state = 'GA'
+ORDER BY weight DESC
