5a1b8c73c71afd338e65f537feeddb7716c6f10e
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-09-17 13:15:47 +0200

    Add join practice.

--- groupby.sql
+++ groupby.sql
@@ -36,3 +36,40 @@ JOIN benn.college_football_teams AS teams
     ON teams.school_name = players.school_name
 WHERE state = 'GA'
 ORDER BY weight DESC
+
+/*
+Write a query that displays player names, school names and conferences for schools
+in the "FBS (Division I-A Teams)" division.
+*/
+
+SELECT player_name, players.school_name, conference
+FROM benn.college_football_players AS players
+JOIN benn.college_football_teams AS teams ON players.school_name = teams.school_name 
+WHERE teams.division = 'FBS (Division I-A Teams)'
+
+/*
+Write a query that performs an inner join between the tutorial.crunchbase_acquisitions table and the tutorial.crunchbase_companies table,
+but instead of listing individual rows, count the number of non-null rows in each table.
+*/
+SELECT COUNT(companies.permalink) AS companies_rowcount,
+       COUNT(acquisitions.company_permalink) AS acquisitions_rowcount
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_acquisitions acquisitions
+    ON companies.permalink = acquisitions.company_permalink
+
+
+    /*
+Count the number of unique companies (don't double-count companies) and unique acquired companies by state.
+Do not include results for which there is no state data, 
+and order by the number of acquired companies from highest to lowest.
+tutorial.crunchbase_acquisitions acquisitions
+
+COUNT UNIQUE permalink AS num_comps, COUNT UNIQUE company_permalink AS num_aq_comps,
+
+*/
+SELECT permalink, COUNT UNIQUE permalink AS num_comps, COUNT UNIQUE company_permalink AS num_aq_comps,
+  FROM tutorial.crunchbase_companies companies
+   LEFT JOIN tutorial.crunchbase_acquisitions acquisitions
+    ON companies.permalink = acquisitions.company_permalink
+WHERE state_code IS NOT NULL
+GROUP BY permalink
