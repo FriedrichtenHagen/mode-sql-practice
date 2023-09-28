0938fb9abcd470d087437edefbf7ccc02fb596fd
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-09-28 16:01:01 +0200

    Finish subquerys.

--- groupby.sql
+++ groupby.sql
@@ -396,3 +396,27 @@ FROM
   ) sub
   GROUP BY 1
   ORDER BY 2 DESC
+
+  /*
+Write a query that does the same thing as in the previous problem, except only for companies that are still operating. 
+Hint: operating status is in tutorial.crunchbase_companies. */
+SELECT
+  investments.investor_name, COUNT(DISTINCT investments.company_permalink)
+FROM tutorial.crunchbase_companies companies
+JOIN 
+  (
+    SELECT
+      *
+    FROM
+      tutorial.crunchbase_investments_part1
+    UNION
+    ALL
+    SELECT
+      *
+    FROM
+      tutorial.crunchbase_investments_part2
+  ) investments 
+  ON companies.permalink = investments.company_permalink
+  WHERE companies.status = 'operating'
+  GROUP BY 1
+  ORDER BY 2 DESC
