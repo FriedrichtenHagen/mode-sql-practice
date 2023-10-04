0c4d796e2b1ce5f9f6ad0f6711f748a51a696e4d
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-10-04 17:55:03 +0200

    rank and dense_rank problem.

--- groupby.sql
+++ groupby.sql
@@ -439,3 +439,48 @@ SELECT start_terminal,
       (SUM(duration_seconds) OVER (Partition by start_terminal ORDER BY start_time))/duration_seconds AS start_terminal_percentage
   FROM tutorial.dc_bikeshare_q1_2012
  WHERE start_time < '2012-01-08'
+
+ /*
+Write a query that shows a running total of the duration of bike rides (similar to the last example),
+but grouped by end_terminal, and with ride duration sorted in descending order.*/
+SELECT end_terminal,
+       duration_seconds,
+       SUM(duration_seconds) OVER
+         (PARTITION BY end_terminal ORDER BY duration_seconds DESC)
+         AS running_total
+  FROM tutorial.dc_bikeshare_q1_2012
+ WHERE start_time < '2012-01-08'
+
+ /*
+ Write a query that shows the 5 longest rides from each starting terminal, ordered by terminal, 
+ and longest to shortest rides within each terminal. Limit to rides that occurred before Jan. 8, 2012.
+ */
+SELECT
+  *
+FROM
+  (
+    SELECT
+      start_terminal,
+      MAX(duration_seconds) OVER (
+        PARTITION BY start_terminal
+        ORDER BY
+          start_terminal
+      ) AS max_ride_duration,
+      MIN(duration_seconds) OVER (
+        PARTITION BY start_terminal
+        ORDER BY
+          start_terminal
+      ) AS min_ride_duration,
+      RANK() OVER (
+        PARTITION BY start_terminal
+        ORDER BY
+          duration_seconds
+      ) AS rank_ride_time,
+      duration_seconds
+    FROM
+      tutorial.dc_bikeshare_q1_2012
+    WHERE
+      start_time < '2012-01-08'
+  ) sub
+WHERE
+  sub.rank_ride_time <= 5
