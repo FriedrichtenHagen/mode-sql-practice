7e362a1ae1e0ee7e45e93e837d99895e3930d223
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-10-05 21:14:37 +0200

    window alias and pivoting.

--- groupby.sql
+++ groupby.sql
@@ -484,3 +484,76 @@ FROM
   ) sub
 WHERE
   sub.rank_ride_time <= 5
+
+
+  /*
+ Write a query that shows only the duration of the trip and the percentile
+ into which that duration falls (across the entire dataset—not partitioned by terminal).
+ */
+
+
+SELECT duration_seconds, NTILE(100) OVER (ORDER BY duration_seconds) AS percentile
+  FROM tutorial.dc_bikeshare_q1_2012
+ WHERE start_time < '2012-01-08'
+ ORDER BY duration_seconds DESC
+
+
+/*
+Window alias
+ */
+
+SELECT start_terminal,
+       duration_seconds,
+       NTILE(4) OVER
+         (PARTITION BY start_terminal ORDER BY duration_seconds)
+         AS quartile,
+       NTILE(5) OVER
+         (PARTITION BY start_terminal ORDER BY duration_seconds)
+         AS quintile,
+       NTILE(100) OVER
+         (PARTITION BY start_terminal ORDER BY duration_seconds)
+         AS percentile
+  FROM tutorial.dc_bikeshare_q1_2012
+ WHERE start_time < '2012-01-08'
+ ORDER BY start_terminal, duration_seconds
+ 
+ 
+ SELECT start_terminal,
+       duration_seconds,
+       NTILE(4) OVER ntile_window AS quartile,
+       NTILE(5) OVER ntile_window AS quintile,
+       NTILE(100) OVER ntile_window AS percentile
+  FROM tutorial.dc_bikeshare_q1_2012
+ WHERE start_time < '2012-01-08'
+WINDOW ntile_window AS
+         (PARTITION BY start_terminal ORDER BY duration_seconds)
+ ORDER BY start_terminal, duration_seconds
+
+
+ /*
+Pivoting
+ */
+SELECT years.*,
+       earthquakes.magnitude,
+       CASE year
+         WHEN 2000 THEN year_2000
+         WHEN 2001 THEN year_2001
+         WHEN 2002 THEN year_2002
+         WHEN 2003 THEN year_2003
+         WHEN 2004 THEN year_2004
+         WHEN 2005 THEN year_2005
+         WHEN 2006 THEN year_2006
+         WHEN 2007 THEN year_2007
+         WHEN 2008 THEN year_2008
+         WHEN 2009 THEN year_2009
+         WHEN 2010 THEN year_2010
+         WHEN 2011 THEN year_2011
+         WHEN 2012 THEN year_2012
+         ELSE NULL END
+         AS number_of_earthquakes
+  FROM tutorial.worldwide_earthquakes earthquakes
+ CROSS JOIN (
+       SELECT year
+         FROM (VALUES (2000),(2001),(2002),(2003),(2004),(2005),(2006),
+                      (2007),(2008),(2009),(2010),(2011),(2012)) v(year)
+       ) years
